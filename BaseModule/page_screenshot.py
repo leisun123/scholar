b9031f9d2264c23f3f30d384d5cf078c5f8b9fc4
IsolationWyn <genius_wz@aliyun.com>
2017-11-12 01:05:50 +0800

    add pj webdriver

--- BaseModule/page_screenshot.py
+++ BaseModule/page_screenshot.py
@@ -1,114 +1,20 @@
-# coding:utf-8
-"""
-@file:      page_screenshot
-@author:    IsolationWyn
-@contact:   genius_wz@aliyun.com
-@python:    3.5.2
-@editor:    PyCharm
-@create:    2017/11/10 21:31
-@description:
-            --
-"""
-# -*- coding: utf-8 -*-
-from queue import Queue
-import os
-import sys
-sys.path.append(os.path.join(os.getcwd().split('scholar')[0],'scholar'))
-import time
-from selenium import webdriver
-from db.SqlHelper import *
-import re
-import redis
-import threading
+from multiprocessing.dummy import Pool
 
-redis_obj = redis.Redis(host='localhost', port=6379, db=5)
-
-
-sqlhelper = SqlHelper(logger=get_logger("test"))
-sc_info_list = iter(sqlhelper.session.query(ObjectAttribute).filter(ObjectAttribute.name == "profile").all())
-try:
-  while True:
-    iter_tmp = next(sc_info_list)
-    sc_info = iter_tmp.decode_value()
-
-    user_obj = sqlhelper.session.query(User).filter(User.object_id == iter_tmp.object_id).first()
-    user_id = user_obj.id if user_obj else None
-    if "website" in sc_info.keys():
-      sc_website = sc_info["website"]
-    else:
-      sc_website = None
-
-    if user_id and sc_website:
-      if re.match(r'^https?:/{2}\w.+$', sc_website):
-        print(sc_website)
-        redis_obj.set(user_id, sc_website)
-except StopIteration as e:
-  print(e)
-finally:
-  redis_obj.save()
-
-driver = webdriver.PhantomJS("D:\Python33\Scripts\phantomjs.exe", service_args=['--disk-cache=yes', '--ignore-ssl-errors=true'])
-def screenshot(id, url):
+import requests
+from BaseModule.url_cache import redis_obj
+def run(url, id):
     try:
-        print(str(url.decode("utf-8")))
-        driver.implicitly_wait(10)
-        driver.get(str(url.decode("utf-8")))
-        driver.save_screenshot('{}.jpg'.format(int(id)))
+        res = requests.post("http://localhost:5050", data={"url":url,"output":"{}.jpg".format(id)})
+        print(url)
     except Exception as e:
         print(e)
-    
-    
+        
+        
+pool = Pool(processes=10)
+
 for i in redis_obj.scan_iter():
-    screenshot(i.decode("utf-8"), redis_obj.get(i))
-    
-    
-    
-# class phantomjs_screenshot():
-#     service_args = ['--disk-cache=yes', '--ignore-ssl-errors=true']
-#     sdk_path = "D:\Python33\Scripts\phantomjs.exe"
-#     max_thread = 10
-#     time_out = 10
-#     sleep_time = 0.00001
-#     th = []
-#
-#     def __init__(self):
-#         self.phantomjs_queue = Queue()
-#         self.url_list = redis_obj.scan_iter()
-#         self.init_phantomjs_driver_pool()
-#
-#
-#     def screenshot(self, url):
-#         driver = self.phantomjs_queue.get()
-#         try:
-#             driver.get(url)
-#         except:
-#             print("Phantomjs Open url Error")
-#
-#         print(driver.current_url)
-#         self.phantomjs_queue.put(driver)
-#
-#     def init_phantomjs_driver_pool(self):
-#         def open_threading():
-#             driver = webdriver.PhantomJS(phantomjs_screenshot.sdk_path, service_args=phantomjs_screenshot.service_args)
-#             driver.implicitly_wait(phantomjs_screenshot.time_out)
-#             phantomjs_screenshot.phantomjs_queue.put(driver)
-#
-#         for i in range(self.max_thread):
-#             t = threading.Thread(target=open_threading)
-#             self.th.append(t)
-#         for i in self.th:
-#             i.start()
-#             time.sleep(self.sleep_time)
-#         for i in self.th:
-#             i.join()
-#
-#     def run(self):
-#         for i in redis_obj.scan_iter():
-#
-#
-# if __name__ == '__main__':
-#     cur = phantomjs_screenshot()
-#     cur.open_phantomjs()
-#     for i in redis_obj.scan_iter():
-#         t =
-    
+    id = int(i.decode('utf-8'))
+    url = str(redis_obj.get(i).decode('utf-8'))
+    pool.apply_async(run, args=(url, id))
+pool.close()
+pool.join()
---
+++ BaseModule/test.js
@@ -0,0 +1,72 @@
+var system = require('system');  //get args
+var args = system.args;
+if (args.length === 2) {
+    var port = Number(args[1]);
+}
+else {
+    var port = 5050;
+}
+var webserver = require('webserver');
+var server = webserver.create()
+var service = server.listen(port, function (request, response) {
+    try {
+
+        var url = request.post["url"];
+        var output = request.post["output"];
+        url = decodeURIComponent(url);
+        // 创建page
+        var webPage = require('webpage');
+        var page = webPage.create();
+        page.settings.resourceTimeout = 20000;//timeout is 20s
+        // 页面错误捕捉
+        page.onError = function (msg, trace) {
+            console.log("[Warning]This is page.onError");
+            var msgStack = ['ERROR: ' + msg];
+            if (trace && trace.length) {
+                msgStack.push('TRACE:');
+                trace.forEach(function (t) {
+                    msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+                });
+            }
+            // console.error(msgStack.join('\n'));
+        };
+        // phantomjs错误捕捉
+        phantom.onError = function (msg, trace) {
+            console.log("[Warning]This is phantom.onError");
+            var msgStack = ['PHANTOM ERROR: ' + msg];
+            if (trace && trace.length) {
+                msgStack.push('TRACE:');
+                trace.forEach(function (t) {
+                    msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function + ')' : ''));
+                });
+            }
+            console.error(msgStack.join('\n'));
+            phantom.exit(1);
+        };
+
+
+        page.open(url, function (status) {
+            // if (status !== 'success') {
+            //     console.log('Unable to load the address!');
+            // } else {
+            //     window.setTimeout(function () {
+            //         page.render(output);
+            //     }, 1000);
+            // }
+            if(status == 'success') {
+                page.render(output)
+                response.status = 200;
+                response.write(page.content)
+            } else {
+                console.log("error")
+                response.write("")
+                response.status = 404;
+            }
+            page.close();
+            response.close()
+        });
+    }
+    catch (e) {
+        console.log('[Error]' + e.message + 'happen' + e.lineNumber + 'line');
+    }
+});
