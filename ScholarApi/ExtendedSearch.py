f68abbd81c55e2cdd575c6d1cdb57f03dd2ecd36
IsolationWyn <genius_wz@aliyun.com>
2018-02-20 18:13:01 +0800

    add pg trigger and jinjasql template

--- ScholarApi/ExtendedSearch.py
+++ ScholarApi/ExtendedSearch.py
@@ -11,6 +11,9 @@
 """
 import sys
 import os
+
+import time
+
 sys.path.append(os.path.join(os.getcwd().split('scholar')[0],'scholar'))
 
 from flask import Flask, jsonify, current_app
@@ -22,9 +25,10 @@ import simplejson
 from sqlalchemy.sql import ClauseElement
 
 from ScholarApi.model import History
+from jinjasql import JinjaSql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wyn:weiaizq1314@localhost:5432/extented_sc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wyn:weiaizq1314@47.254.34.29:5432/extented_sc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['FLASKY_SLOW_DB_QUERY_TIME'] = 2
 
@@ -34,135 +38,82 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-#@app.after_request
-#def after_request(response):
-#    for query in get_debug_queries():
-#        if query.duration >= current_app.config["FLASKY_SLOW_DB_QUERY_TIME"]:
-#            current_app.logger.warning(
-#                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n' %\
-#                    (query.statement, query.parameters, query.duration,query.context))
-#    return response
+j = JinjaSql()
 
-# from sqlalchemy.ext.declarative import DeclarativeMeta
-# class AlchemyEncoder(simplejson.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj.__class__, DeclarativeMeta):
-#             # an SQLAlchemy class
-#             fields = {
-#                 "id",
-#                 "title",
-#                 ""
-#             }
-#             for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-#                 data = obj.__getattribute__(field)
-#                 try:
-#                     simplejson.dumps(data) # this will fail on non-encodable values, like other classes
-#                     fields[field] = data
-#                 except TypeError:
-#                     fields[field] = None
-#         # a json-encodable dict
-#             return fields
-#         return simplejson.JSONEncoder.default(self, obj)
 
 @app.route("/api/search", methods=["GET"])
 def search():
     
         res = {}
         res["state"] = {}
-    
-        author = request.args.get("author")
-        thesis = request.args.get("thesis")
-        keywords = request.args.get("keywords")
-        if isinstance(keywords, list):
-            keywords = " ".join(keywords)
-        offset = request.args.get("offset")
-        limit = request.args.get("limit")
-        user_id = request.args.get("userId")
-        year_begin = request.args.get("yearBegin")
-        year_end = request.args.get("yearEnd")
+        
+        data = request.args.to_dict()
+        print(data)
+        
+        template = """
+                  SELECT  id, title, year, link, bibtex, resource_type, resource_link, summary, google_id, pdf_temp_url, site, keywords, authors,
+                  {% if author is defined %}
+                    similarity(authors, {{ author }} ) AS dist
+                  {% elif thesis is defined %}
+                    similarity(thesis, {{ thesis }} ) AS dist
+                  {% elif keywords is defined %}
+                    similarity(keywords, {{ keywords }} ) AS dist
+                  {% endif %}
+                  
+                  FROM articles WHERE
+                  LENGTH(authors) < 100
+                  
+                  AND year BETWEEN
+                  
+                  {% if year_begin is defined %}
+                    {{ year_begin | sqlsafe }}
+                  {% else %}
+                    1900
+                  {% endif %}
+                  
+                  {% if year_end is defined %}
+                  AND {{ year_end | sqlsafe }}
+                  {% else %}
+                  AND  2020
+                  {% endif %}
+                  
+                  {% if author is defined %}
+                  AND authors ~ \'{{ author | sqlsafe }}\'
+                  {% endif %}
+                  
+                  {% if thesis is defined %}
+                  AND thesis ~ {{ thesis | sqlsafe}}
+                  {% endif %}
+                  
+                  {% if keywords is defined %}
+                  AND keywords ~ {{ keywords | sqlsafe }}
+                  {% endif %}
+                  
+                  ORDER BY dist
+                  {% if offset and limit %}
+                  DESC OFFSET {{ offset | sqlsafe }} LIMIT {{ limit | sqlsafe }}
+                  {% endif %}
+                  ;
+                   """
         
         
-        nonon_params = []
         
-        if len(author):
-            nonon_params.append(("authors",  author))
-        if len(thesis):
-            nonon_params.append(("title", thesis))
-        if len(keywords):
-            nonon_params.append(("keywords", keywords))
-        
-        if len(nonon_params) == 0:
-            res["state"] = {
-                "code": 500,
-                "msg": "error"
+        query, bind_params = j.prepare_query(template, data)
+        result = db.engine.execute(query, *bind_params).fetchall()
+
+        count = 0
+        for index, line in enumerate(result):
+            count += 1
+            
+        return simplejson.dumps(
+            {
+                "count": count,
+                "data": [dict(i) for i in result]
             }
-            return jsonify(res)
+        )
+   
         
-        else:
-            query = ' AND '.join(map(lambda  x: "{} ~ '{}'".\
-                    format(x[0], x[1]) , nonon_params))
-            
-            year_filter = "year < {} AND year > {} AND".format(year_end if year_end else 2018, \
-                                                           year_begin if year_begin else 1900)
-            
-            sql = "SELECT * , similarity({}, '{}') AS dist \
-                        from articles WHERE {} {}\
-                        ORDER BY {} DESC OFFSET {} LIMIT {}; \
-                        " \
-                    .format(nonon_params[0][0], \
-                            nonon_params[0][1], \
-                            year_filter,
-                            query, \
-                            "dist", \
-                            offset,
-                            limit)
-            
-            result = db.engine.execute(sql).fetchall()
-            res["data"] = []
-            res["state"]["code"] = 200
-            res["state"]["msg"] = "ok"
-            res["count"] = len(result)
-            
-            tmp = iter(result)
-            try:
-                while True:
-                    i = next(tmp)
-                    
-                    
-                #db.session.add(
-                #    History(
-                #        user_id = user_id,
-                #        article_id = i.id
-                #    )
-                #)
-                    if  len(str(i.authors)) < 100:
-                        authors = (i.authors).replace("\u00a0", "").replace("\"\"","").split(",")
-                        #print(authors)
-                        res["data"].append(
-                            {
-                            "id": i.id,
-                            "title": i.title,
-                            "year": i.year,
-                            "link": i.link,
-                            "bibtex": i.bibtex,
-                            "summary": i.summary,
-                            "google_id": i.google_id,
-                            "pdf_temp_url": i.pdf_temp_url,
-                            "keywords": i.keywords,
-                            "authors": authors
-                                }
-                            )
-            except StopIteration:
-                return jsonify(res)
-            # except Exception as e:
-            #     res["state"] =  {
-            #             "code": 408,
-            #             "msg": "timeout"
-            #             }
-            #     print(e)
-            #     return jsonify(
-            #             res
-            #         )
+        
 
 def get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
@@ -176,5 +127,4 @@ def get_or_create(session, model, **kwargs):
 
 if __name__ == '__main__':
     app.run(debug=True, port=2000)
-    #manager.run()
     
--- ScholarApi/sc.sql
+++ ScholarApi/sc.sql
@@ -2,4 +2,37 @@ CREATE EXTENSION pg_trgm;
 CREATE INDEX on articles USING GIN (authors gin_trgm_ops);
 CREATE INDEX on articles USING GIST (authors gist_trgm_ops);
 
-select authors, similarity(authors, 'Jerry') as dist  from articles where authors ~ 'Jerry' order by dist desc
+select authors, similarity(authors, 'Jerry') as dist  from articles where authors ~ 'Jerry' order by dist；
+
+
+ create table articles_count(col int);
+
+ insert into articles_count ( select count(*) from articles);
+
+CREATE OR REPLACE FUNCTION articles_insert_func()  RETURNS TRIGGER AS $$
+        BEGIN
+                update  articles_count  set col = col + 1;
+                RETURN null;
+        END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION articles_delete_func()  RETURNS TRIGGER AS $$
+        BEGIN
+                update articles_count set col = col - 1;
+                RETURN null;
+        END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER articles_insert
+    AFTER INSERT ON articles
+    FOR EACH ROW
+    EXECUTE PROCEDURE articles_insert_func();
+
+CREATE TRIGGER articles_delete
+    AFTER DELETE ON articles
+    FOR EACH ROW
+    EXECUTE PROCEDURE articles_delete_func()
+
+
